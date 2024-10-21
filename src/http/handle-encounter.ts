401a7276235db2233f018fd59b94467584511f76
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2024-10-21 17:59:32 -0300

    fix: improve error treatmenton encounter

--- src/http/handle-encounter.ts
+++ src/http/handle-encounter.ts
@@ -4,23 +4,20 @@ import { clientEnv } from '@/env'
 import { cookies } from 'next/headers'
 
 export async function handleEncounter(encounterData: FormData) {
-  try {
-    const request = await fetch(
-      `${clientEnv.NEXT_PUBLIC_BACKEND_URL}/encounter`,
-      {
-        method: 'POST',
-        cache: 'no-cache',
-        credentials: 'include',
-        headers: { Cookie: cookies().toString() },
-        body: encounterData,
-      }
-    )
-
-    if (request.status !== 200) {
-      throw new Error('Invalid encounter.')
+  const request = await fetch(
+    `${clientEnv.NEXT_PUBLIC_BACKEND_URL}/encounter`,
+    {
+      method: 'POST',
+      cache: 'no-cache',
+      credentials: 'include',
+      headers: { Cookie: cookies().toString() },
+      body: encounterData,
     }
-  } catch (e) {
-    console.log(e)
-    throw new Error()
+  )
+
+  if (!request.ok) {
+    const errorMessage = await request.text()
+    console.error('Erro no backend:', errorMessage)
+    throw new Error('Invalid encounter.')
   }
 }
